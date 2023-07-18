2f551cc71eeaf86c571e75888ee59664e53e2413
Kot-Kokoss <izaostrovskiy@mail.ru>
2023-07-18 18:33:18 +0500

    map+anim
    added map, collisions, swap animation

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -10,13 +10,128 @@ export class Game extends Phaser.Scene {
     }
 
     create() {
-        const map = this.make.tilemap({key: "dungeon"});
-        const tileset = map.addTilesetImage('Tiles', 'tiles')
+        //map
+        const map = this.make.tilemap({ key: "dungeon" });
+        const tileset = map.addTilesetImage('Tiles', 'tiles_file');
 
-        const ground = map.createLayer('Ground', tilesetkkmknmnjnj)
+        const ground = map.createLayer('Ground', tileset);
+        const walls = map.createLayer('Walls', tileset);
+        const decor = map.createLayer('Decor', tileset);
+
+        walls.setCollisionByProperty({ collides: true });
+        decor.setCollisionByProperty({ collides: true });
+
+        // chek collision
+        const debugGraphics = this.add.graphics().setAlpha(0.7);
+        walls.renderDebug(debugGraphics, {
+            tileColor: null,
+            collidingTleColor: new Phaser.Display.Color(231, 222, 48, 253),
+            faceColor: new Phaser.Display.Color(231, 222, 56, 213),
+        });
+        decor.renderDebug(debugGraphics, {
+            tileColor: null,
+            collidingTleColor: new Phaser.Display.Color(231, 222, 48, 253),
+            faceColor: new Phaser.Display.Color(231, 222, 56, 213),
+        });
+        //enemy
+        this.goblin = this.add.sprite(250, 300, "goblin_idle");
+        this.goblin.play("goblin_idle_anim");
+
+        this.goblin.setInteractive();
+
+        this.input.on('gameobjectdown', this.hit, this);
+
+        this.hero = this.physics.add.sprite(260, 320, "hero_idle");
+
+        this.hero.play("hero_idle_anim");
+
+        this.physics.add.collider(this.hero, walls);
+        this.physics.add.collider(this.hero, decor);
+
+        this.cursorKeys = this.input.keyboard.createCursorKeys();
+        this.analogCursorKeys = this.input.keyboard.addKeys({
+            w: Phaser.Input.Keyboard.KeyCodes.W,
+            a: Phaser.Input.Keyboard.KeyCodes.A,
+            s: Phaser.Input.Keyboard.KeyCodes.S,
+            d: Phaser.Input.Keyboard.KeyCodes.D
+        })
+
+        
     }
 
     update() {
+        this.moveHeroManager();
+    }
 
+    hit(pointer, gameObject) {
+        gameObject.setTexture("hit");
+        gameObject.play("hit_anim");
+    }
+
+    moveHeroManager() {
+        this.hero.setVelocity(0);
+
+        if (Phaser.Input.Keyboard.JustDown(this.cursorKeys.up)) {
+            this.hero.play("hero_run_anim", true);
+        }
+        if (Phaser.Input.Keyboard.JustUp(this.cursorKeys.up)) {
+            this.hero.play("hero_idle_anim", true);
+        }
+        if (Phaser.Input.Keyboard.JustDown(this.cursorKeys.left)) {
+            this.hero.play("hero_run_anim", true);
+        }
+        if (Phaser.Input.Keyboard.JustUp(this.cursorKeys.left)) {
+            this.hero.play("hero_idle_anim", true);
+        }
+        if (Phaser.Input.Keyboard.JustDown(this.cursorKeys.down)) {
+            this.hero.play("hero_run_anim", true);
+        }
+        if (Phaser.Input.Keyboard.JustUp(this.cursorKeys.down)) {
+            this.hero.play("hero_idle_anim", true);
+        }  
+        if (Phaser.Input.Keyboard.JustDown(this.cursorKeys.right)) {
+            this.hero.play("hero_run_anim", true);
+        }
+        if (Phaser.Input.Keyboard.JustUp(this.cursorKeys.right)) {
+            this.hero.play("hero_idle_anim", true);
+        } 
+        if (Phaser.Input.Keyboard.JustDown(this.analogCursorKeys.w)) {
+            this.hero.play("hero_run_anim", true);
+        } 
+        if (Phaser.Input.Keyboard.JustUp(this.analogCursorKeys.w)) {
+            this.hero.play("hero_idle_anim", true);
+        } 
+        if (Phaser.Input.Keyboard.JustDown(this.analogCursorKeys.a)) {
+            this.hero.play("hero_run_anim", true);
+        } 
+        if (Phaser.Input.Keyboard.JustUp(this.analogCursorKeys.a)) {
+            this.hero.play("hero_idle_anim", true);
+        } 
+        if (Phaser.Input.Keyboard.JustDown(this.analogCursorKeys.s)) {
+            this.hero.play("hero_run_anim", true); 
+        } 
+        if (Phaser.Input.Keyboard.JustUp(this.analogCursorKeys.s)) {
+            this.hero.play("hero_idle_anim", true); 
+        } 
+        if (Phaser.Input.Keyboard.JustDown(this.analogCursorKeys.d)) {
+            this.hero.play("hero_run_anim", true);
+        }
+        if (Phaser.Input.Keyboard.JustUp(this.analogCursorKeys.d)) {
+            this.hero.play("hero_idle_anim", true);
+        }
+
+        if (this.cursorKeys.left.isDown || this.analogCursorKeys.a.isDown) {
+            this.hero.setScale(-1, 1);
+            this.hero.setVelocityX(-70);
+        } else if (this.cursorKeys.right.isDown || this.analogCursorKeys.d.isDown) {
+            this.hero.setScale(1, 1);
+            this.hero.setVelocityX(70);
+        }
+
+        if (this.cursorKeys.up.isDown || this.analogCursorKeys.w.isDown) {
+            this.hero.setVelocityY(-70);
+        } else if (this.cursorKeys.down.isDown || this.analogCursorKeys.s.isDown) {
+            this.hero.setVelocityY(70);
+        }
     }
 }
--- src/scenes/preloader.js
+++ src/scenes/preloader.js
@@ -6,11 +6,64 @@ export class Preloader extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('tiles', 'src/img/assets/full tilemap.png')
+        this.load.image('tiles_file', 'src/img/assets/full_tilemap.png')
         this.load.tilemapTiledJSON('dungeon', 'src/img/assets/tileMap.json')
+
+        this.load.spritesheet("hero_idle", "src/img/assets/spritesheets/hero_idle_spritesheet.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        });
+        this.load.spritesheet("hero_run", "src/img/assets/spritesheets/hero_run_spritesheet.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        });
+        this.load.spritesheet("goblin_idle", "src/img/assets/spritesheets/goblin_idle_spritesheet.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        })
+        this.load.spritesheet("goblin_run", "src/img/assets/spritesheets/goblin_run_spritesheet.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        })
+        this.load.spritesheet("hit", "src/img/assets/spritesheets/hit_effect_anim_spritesheet.png", {
+            frameWidth: 8,
+            frameHeight: 8
+        })
     }
 
     create() {
         this.scene.start("game");
+
+        this.anims.create({
+            key: "hero_idle_anim",
+            frames: this.anims.generateFrameNumbers("hero_idle"),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "hero_run_anim",
+            frames: this.anims.generateFrameNumbers("hero_run"),
+            frameRate: 20,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "goblin_idle_anim",
+            frames: this.anims.generateFrameNumbers("goblin_idle"),
+            frameRate: 5,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "goblin_run_anim",
+            frames: this.anims.generateFrameNumbers("goblin_run"),
+            frameRate: 20,
+            repeat: -1
+        })
+        this.anims.create({
+            key: "hit_anim",
+            frames: this.anims.generateFrameNumbers("hit"),
+            frameRate: 15,
+            repeat: 0,
+            hideOnComplete: true
+        });
     }
 }
